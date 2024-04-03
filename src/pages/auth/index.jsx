ebc2b591fe5431431f1a8411e4a23f475e83b7c7
ZunainAliAzam <zunainazam456@gmail.com>
2024-04-04 03:42:32 +0500

    google authentication done

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,29 +1,56 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, provider } from "../../config/firebase-config";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import "./index.css";
 
 export const Auth = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
 
+  const navigate = useNavigate();
   const signInWithGoogle = async () => {
     const results = await signInWithPopup(auth, provider);
     console.log(results);
+    const authInfo = {
+      userID: results.user.uid,
+      name: results.user.displayName,
+      profilePhoto: results.user.photoURL,
+      isAuth: true,
+    };
+
+    localStorage.setItem("auth", JSON.stringify(authInfo));
+    navigate("/expense");
   };
-  const navigate = useNavigate();
 
   const onButtonClick = () => {
-    // You'll update this function later...
+    let isValid = true;
+
+    if (!email) {
+      isValid = false;
+    }
+    if (!password) {
+      isValid = false;
+    }
+
+    if (isValid) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log("User logged in:", user);
+          navigate("/expense");
+        })
+        .catch((error) => {
+          const errorMessage = error.message;
+          console.log("Error message:", errorMessage);
+        });
+    }
   };
 
   return (
     <div className={"mainContainer"}>
       <div className={"titleContainer"}>
-        <div>Login</div>
+        <div>Expense Tracker Login</div>
       </div>
       <br />
       <div className={"inputContainer"}>
@@ -33,7 +60,6 @@ export const Auth = (props) => {
           onChange={(ev) => setEmail(ev.target.value)}
           className={"inputBox"}
         />
-        <label className="errorLabel">{emailError}</label>
       </div>
       <br />
       <div className={"inputContainer"}>
@@ -43,7 +69,6 @@ export const Auth = (props) => {
           onChange={(ev) => setPassword(ev.target.value)}
           className={"inputBox"}
         />
-        <label className="errorLabel">{passwordError}</label>
       </div>
       <br />
       <div className={"inputContainer"}>
@@ -56,10 +81,10 @@ export const Auth = (props) => {
       </div>
       <div className="inputGoogle">
         <input
-            className={"googleButton"}
-            type="button"
-            onClick={signInWithGoogle}
-            value={"Sign in with Google"}
+          className={"googleButton"}
+          type="button"
+          onClick={signInWithGoogle}
+          value={"Sign in with Google"}
         />
       </div>
     </div>
