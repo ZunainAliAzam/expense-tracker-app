414338aab01d5e5e5b91a526a986b402bfcc9ee1
ZunainAliAzam <zunainazam456@gmail.com>
2024-04-04 08:57:10 +0500

    expense page initialized

--- src/pages/expense-page/index.jsx
+++ src/pages/expense-page/index.jsx
@@ -1,3 +1,38 @@
 export const ExpenseTracker = () => {
-    return <div>hello world expense tracker</div>
-}
+  return (
+    <>
+      <div className="expense-tracker">
+        <div className="container">
+          <h1>Expense Tracker</h1>
+          <div className="balance">
+            <h3>Your Balance</h3>
+            <h2>$0.00</h2>
+          </div>
+        </div>
+        <div className="summary">
+          <div className="income">
+            <h4>Income</h4>
+            <p>$0.00</p>
+          </div>
+          <div className="expenses">
+            <h4>Expenses</h4>
+            <p>$0.00</p>
+          </div>
+        </div>
+        <form className="add-transaction">
+          <input type="text" placeholder="Description" required />
+          <input type="text" placeholder="Amount" required />
+          <input type="radio" id="expense" value="expense" />
+          <label htmlFor="expense">Expense </label>
+          <input type="radio" id="income" value="income" />
+          <label htmlFor="income">Income </label>
+
+          <button type="submit">Add Transaction</button>
+        </form>
+      </div>
+      <div className="transactions">
+        <h3>Transactions</h3>
+      </div>
+    </>
+  );
+};
