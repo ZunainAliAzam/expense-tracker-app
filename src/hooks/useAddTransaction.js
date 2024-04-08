4ef8cdc897bd642e65c71912ab751a1d4f9c3a61
ZunainAliAzam <zunainazam456@gmail.com>
2024-04-08 12:28:37 +0500

    connected firestore

---
+++ src/hooks/useAddTransaction.js
@@ -0,0 +1,18 @@
+import {addDoc, collection, serverTimestamp} from "firebase/firestore"
+import {db} from '../config/firebase-config'
+
+export const useAddTransaction = () => {
+
+    const transactionCollectionRef = collection(db, "Transactions")
+
+    const AddTransaction = async () => {
+        await addDoc(transactionCollectionRef,{
+            userID:"",
+            description:"",
+            transactionAmount:0,
+            transactionTypes:"",
+            createdAt:serverTimestamp
+        })
+    }
+    return {AddTransaction}
+}
