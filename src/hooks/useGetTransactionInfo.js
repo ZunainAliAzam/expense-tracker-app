9bd94af0374bef2e1dfb7348af2ebbfaedfa4d43
ZunainAliAzam <zunainazam456@gmail.com>
2024-04-09 09:34:47 +0500

    get usser data by quering through userId

--- src/hooks/useGetTransactionInfo.js
+++ src/hooks/useGetTransactionInfo.js
@@ -1,13 +1,24 @@
 import { useState, useEffect } from 'react';
+import { query, collection, where, orderBy } from "firebase/firestore"
+import { db } from "../config/firebase-config"
+import { useGetUserInfo } from "./useGetUserInfo"
 
 export const useGetTransactionInfo = () => {
 
+    const { userID } = useGetUserInfo()
+    const transactionCollectionRef = collection(db, "transactions")
+
     const [transactions, setTransactions] = useState([])
     const getTransactions = async () => {
-        try { 
-
-        } catch (err) { 
-            console.error(err); 
+        try {
+            const queryTransactions = query(
+                transactionCollectionRef, 
+                where("userID", "==", userID),
+                orderBy("createdAt")
+            )
+            
+        } catch (err) {
+            console.error(err);
         }
     }
 
